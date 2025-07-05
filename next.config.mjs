97ca19a7ea31b78cc4e42419bdfa649ea4b0935b
UBOT9 <121751956+UBOT9@users.noreply.github.com>
2025-07-05 17:54:25 +0530

    Added .babelrc

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    swcMinify: false,
+    experimental: {
+        forceSwcTransforms: false,
+    },
     webpack: config => {
         config.resolve.alias.canvas = false;
         return config;
